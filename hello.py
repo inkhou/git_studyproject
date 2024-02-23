9bac085c2714041f638085bc6fb32f8b1492377e
kennyhou <776302373@qq.com>
2024-02-23 05:09:56 -0500

    other_user add test1 fuc

--- hello.py
+++ hello.py
@@ -4,9 +4,15 @@ def test(a):
 	b = type(a)
 	return b
 
+def test1(a,b)
+    c = sum(a,b)
+    return c
+
 def main():
-	result = test("123")
-	print(result)
+	result1 = test("123")
+    result2 = test(1,2)
+	print(result1)
+    print(result2)
 
 if __name__ == '__main__':
 	main()
